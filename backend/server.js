a2b3138bdcd438ca349c145c2fb3916d89c12d27
Mattias Ekendahl <mattias@developedbyme.com>
2026-06-02 13:21:08 +0200

    Update: notes about every step

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,31 @@
+//Importera bibliotek
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+
+//Importera egna filer
 import productsRouter from "./product-routes.js";
 
+//Ladda in .env variabler
 dotenv.config();
 
+//Första konsolllogg bara för att se att node startar
 console.log("Start");
 
+//Skapa en server
 let app = express();
 
+//Använd moduler för att parsa body i POST-anrop
 app.use(express.json());
 app.use(express.urlencoded());
+
+//Använd cors så att vi kan göra requests från vite
 app.use(cors());
+
+//Använd vår egen products router
 app.use("/api/products", productsRouter);
 
+//Lägg till endpoints direkt på servern
 app.get("/", (req, res) => {
     res.send("Hello world<form method='POST'><input type='submit' /></form>");
 });
@@ -32,8 +44,10 @@ app.get("/api/count/", (req, res) => {
     res.json({"count": count});
 });
 
+//Använd port från .env eller 3000 om inte angiven
 let port = process.env.PORT || 3000;
 
+//Starta webservern
 app.listen(port, () => {
     console.log("Listening to " + port);
 })
